a91f535bd7557541ece1829387ab0440a3be7cf0
vcv8 <vcv183@gmail.com>
2024-11-05 01:17:37 +0100

    fix: :bulb: Edit TODO

--- test/feature/city_search/cubit/city_search_cubit/city_search_cubit_test.dart
+++ test/feature/city_search/cubit/city_search_cubit/city_search_cubit_test.dart
@@ -2,6 +2,6 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('city search cubit ...', (tester) async {
-    // TODO: Implement test
+    // TODO(Victor): Implement test
   });
 }
